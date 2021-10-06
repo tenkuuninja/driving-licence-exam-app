ca4827f89fce58ea85f09f89e61e1badd1061637
tenkuuninja <27910240+tenkuuninja@users.noreply.github.com>
2021-10-06 10:24:49 +0700

    add data question topic

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,7 +1,8 @@
 import signalData from './data_signal.json';
 import signalTopicData from './data_signal_topic.json';
-import lawData from './data_law.json';
 import lawTopicData from './data_law_topic.json';
+import lawData from './data_law.json';
+import questionTopicData from './data_question_topic.json';
 import questionsData from './data_question.json';
 import { ILaw, IQuestion, ISign, ITopic } from '../interface';
 
@@ -19,6 +20,10 @@ export const getLawTopic = (): ITopic[] => {
 
 export const getLawByVehicleCodeAndTopicId = (vehicleCode: number, topicId: number): ILaw[] => {
   return lawData.filter((item: ILaw) => item.vehicleCode === vehicleCode && item.topicId === topicId)
+}
+
+export const getQuestionTopic = (): ITopic[] => {
+  return questionTopicData;
 }
 
 export const getAllQuestions = (): IQuestion[] => questionsData
